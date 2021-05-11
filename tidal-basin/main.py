cca742dda9b14978aa829ea7b04b6e05d79921e2
Some Two <some@one.com>
2021-05-11 11:43:59 +0000

    Automated running all simulations; CFL errors per simulation distilled in general log

--- tidal-basin/main.py
+++ tidal-basin/main.py
@@ -1,10 +1,11 @@
 from simulations import simulation_parameters
 
-from jinja2 import Template
+import jinja2
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import datetime
 
 from pathlib import Path
 import os
@@ -23,6 +24,12 @@ class swash_sim(object):
         self.fpath_runfile = self.rundir / f'runfile_{self.sim_id}.sws'
         self.fpath_output_data = self.rundir / f"output_buoy_{self.sim_id}.tbl"
 
+        self.image_folder = self.rundir.absolute().parent / 'images'
+        self.image_folder.mkdir(parents=True, exist_ok=True)
+        self.fpath_figure_jpg = self.image_folder / f"figure_{self.sim_id}.jpg"
+
+        self.fpath_print_file = self.rundir / f'runfile_{self.sim_id}.prt'
+
         if not (self.rundir / 'swashinit').exists():
             src = self.rundir.absolute().parent / 'swashinit'
             dst = self.rundir.absolute() / 'swashinit'
@@ -38,7 +45,7 @@ class swash_sim(object):
 
     def _create_sws_command_file(self):
         with open(self.fname_jinja_base) as base:
-            template = Template(base.read())
+            template = jinja2.Template(base.read())
 
         template.stream(sim=self).dump(str(self.fpath_runfile.absolute()))
 
@@ -102,13 +109,50 @@ class swash_sim(object):
     def plot_multi_index(self):
         df = self._get_data()
         n = self.output_nodes+1
-        df.unstack(level=1).plot(sort_columns=True, subplots=True, grid=True, title=f"Velocity and waterlevel over time for {n} locations", legend=True)
+        axes = df.unstack(level=1).plot(sort_columns=True, subplots=True, grid=True, title=f"Simulation: {self.sim_id}; Velocity and waterlevel over time for {n} locations", legend=True)
+        for ax in axes:
+            ax.legend(loc='lower right')
+        plt.savefig(self.fpath_figure_jpg)
         #plt.suptitle(f'Simulation: {self.sim_id}')
         #df.plot(kind='line', subplots=True, grid=True, title="Sample Data (Unit)",
                 #layout=(4, 3), sharex=True, sharey=False, legend=True,    
                 #style=['r', 'r', 'r', 'g', 'g', 'g', 'b', 'b', 'b', 'r', 'r', 'r'],
                 #xticks=np.arange(0, len(df), 16))
 
+    
+    def full_run(self):
+        self.prepare_run()
+        self.run_swash()
+        self.plot_multi_index()
 
+
+
+def run_all_simulations():
+    ## Create log file
+    log_messages = []
+    ## Run simulations
+    for k,params in simulation_parameters.__dict__.items():
+        if not k.startswith('tid'):
+            continue
+
+        sim = swash_sim(**params)
+        log_messages.append(f'\n Started new simulation - {sim.sim_id}')
+        sim.full_run()
+
+        
+        error_msg = '** Warning          : CFL condition is violated!'
+        with open(sim.fpath_print_file) as f:
+            n = f.read().count(error_msg)
+            if n:
+                log_messages.append(f'[{n} CFL errors] - {error_msg}')
+
+    
+    logpath = Path('.') / ('errors-%s.log' % datetime.datetime.now().strftime('%y%m%d_%H%M%S'))
+    with open(logpath.absolute(), 'w+') as f:
+        f.writelines([str(l)+ '\n' for l in log_messages])
+
+
+
+        
 
 
--- tidal-basin/simulations.py
+++ tidal-basin/simulations.py
@@ -3,76 +3,86 @@ import numpy as np
 
 class simulation_parameters(object):
     # Tidal Basin
-    tidbas001 = dict(
+    tidbas000 = dict(
             # general
             name = 'Tidal basin',
-            sim_id = '001',
+            sim_id = '000',
             description = 'Modelling free and force behaviour of a tidal basin',
             fname_jinja_base = 'tidal_basin_base.jinja',
+
             # Grid spec
             basinlenght = 10000,
             xm = 100,
+
+            # Initial conditions
+            initial_cond = False,  # -> Means INIT ZERO
+            # initial_cond = True -> Means velocities are derived from waterlevel, must specify initial water level elsewhere via READINP.
+            # initial_cond = dict(wlev=1, vx=1, vy=1, tke=1, epsilon=1) -> all keys are optional
+
             # boundary conditions
             tidal_amp = 1,
             tidal_freq = np.pi*2/(12*3600),
             tidal_phase = 90,
+
             # bottom
-            # Possibly construct .bot file from bottom definition
+            # constructs .bot file from bottom_list
             bottom_list = [10, 10],
             fname_bottom = 'flat.bot',
             swashfname = 'tidal-basin.sws',
+
+            # Friction
+            friction_type = 'MANNING', #[CONSTANT, MANNING, CHEZY, COLEBROOK]
+            frictin_val = 0.019, 
+
             # computation
             starttime = dt.datetime(2021, 4, 20),
             simulation_time = dt.timedelta(days=5),
-            timestep = 5,
+            timestep = 1,
             timestep_unit = 'MIN',
+
             # output
             output_params = ['WATLEV', 'VMAG'],
             output_start = 0, # 0 -> starttime else specify datetime or timedelta 
             output_freq = 5,
             output_freq_unit = 'MIN', #SEC MIN HR DAY
-            output_nodes = 5
+            output_nodes = 3
             )
 
-    tidbas002 = tidbas001.copy()
+    tidbas001 = tidbas000.copy()
+    tidbas001['sim_id'] = '001'
+    tidbas001['bottom_list'] = [5, 5]
+
+    tidbas002 = tidbas000.copy()
     tidbas002['sim_id'] = '002'
-    tidbas002['depth'] = 50000
+    tidbas002['bottom_list'] = [20, 20]
 
-    tidbas002 = tidbas001.copy()
-    tidbas002['sim_id'] = '002'
-    tidbas002['basinlenght'] = 50000
-
-    tidbas002 = tidbas001.copy()
-    tidbas002['sim_id'] = '002'
-    tidbas002['basinlenght'] = 50000
-
-    tidbas002 = tidbas001.copy()
-    tidbas002['sim_id'] = '002'
-    tidbas002['basinlenght'] = 50000
-
-    tidbas002 = tidbas001.copy()
-    tidbas002['sim_id'] = '002'
-    tidbas002['basinlenght'] = 50000
-
-    tidbas002 = tidbas001.copy()
-    tidbas002['sim_id'] = '002'
-    tidbas002['basinlenght'] = 50000
-
-    tidbas002 = tidbas001.copy()
-    tidbas002['sim_id'] = '002'
-    tidbas002['basinlenght'] = 50000
-
-    tidbas002 = tidbas001.copy()
-    tidbas002['sim_id'] = '002'
-    tidbas002['basinlenght'] = 50000
-
-    tidbas002 = tidbas001.copy()
-    tidbas002['sim_id'] = '002'
-    tidbas002['basinlenght'] = 50000
-
-
-    tidbas003 = tidbas001.copy()
+    tidbas003 = tidbas000.copy()
     tidbas003['sim_id'] = '003'
-    tidbas003['basinlenght'] = 5000
+    tidbas003['friction_type'] = None
+    tidbas003['friction_val'] = None
 
+    tidbas004 = tidbas000.copy()
+    tidbas004['sim_id'] = '004'
+    tidbas004['friction_type'] = 'MANNING'
+    tidbas004['friction_val'] = 0.04 # m^-1/3 s
 
+    tidbas005 = tidbas000.copy()
+    tidbas005['sim_id'] = '005'
+    tidbas005['friction_type'] = 'COLEBROOK'
+    tidbas005['friction_val'] = 0.01  # m
+ 
+    tidbas006 = tidbas000.copy()
+    tidbas006['sim_id'] = '006'
+    tidbas006['basinlenght'] = 5000
+
+    tidbas007 = tidbas000.copy()
+    tidbas007['sim_id'] = '007'
+    tidbas007['basinlenght'] = 50000
+
+    tidbas008 = tidbas000.copy()
+    tidbas008['sim_id'] = '008'
+    tidbas008['simulation_time'] = dt.timedelta(days=15)
+
+    tidbas009 = tidbas000.copy()
+    tidbas009['sim_id'] = '009'
+    tidbas009['simulation_time'] = dt.timedelta(days=30)
